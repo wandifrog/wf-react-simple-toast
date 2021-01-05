d589b558b612dba1225bda7458bf3317034d3101
Wandi <wandifrog@gmail.com>
2021-01-06 06:30:15 +0700

    chore: update entire code

--- src/index.js
+++ src/index.js
@@ -1,44 +1,29 @@
-let defaultTimeToClose = 2000
-
-const toastContainer = {
+const toastContainerStyle = {
   position: 'fixed',
   right: 0,
   top: 0
 }
 
-const toast = {
-  display: 'flex',
-  justifyContent: 'flex-end',
-  margin: '10px 10px 0 0',
-  maxWidth: '300px'
-}
+const Toast = () => <div style={toastContainerStyle} id="toast-container" />
 
-const toastMessage = {
-  padding: '15px',
-  borderRadius: '10px',
-  backgroundColor: '#eee'
-}
-
-function ToastContainer(props) {
-  defaultTimeToClose = props.timeToClose
-  return (
-    <div style={toastContainer} id="toast-container" />
-  )
-}
-
-function Toast(message, timeToClose = defaultTimeToClose) {
+function toastMessage(message, timeToClose = 2000) {
   const container = document.getElementById('toast-container')
-  let randomId = 'toast=' + Math.ceil((Math.random() * 100000))
-  if (!container.children.length) {
-    container.innerHTML += `<div id="${randomId}" style={${toast}}><span style={${toastMessage}}>${message}</span></div>`
-  } else {
-    const copy = container.innerHTML
-    container.innerHTML = `<div id="${randomId}" style={${toast}}><span style={${toastMessage}}>${message}</span></div>${copy}`
-  }
+  const randomId = 'toast=' + Math.ceil((Math.random() * 100000))
+  const toastMessageElement = `
+    <div id="${randomId}" 
+      style="display: flex; justify-content: flex-end; margin: 10px 10px 0 0; max-wdith: 300px">
+      <span style="padding: 15px; border-radius: 10px; background: #eee">${message}</span>
+    </div>
+  `
+  if (!container) return
+
+  const oldToastMessageElement = container.innerHTML
+  container.innerHTML = toastMessageElement + oldToastMessageElement
   setTimeout(() => {
-    document.getElementById(randomId).remove()
+    document.getElementById(randomId) && document.getElementById(randomId).remove()
   }, timeToClose)
 }
 
+Toast.toastMessage = toastMessage
 
-export { ToastContainer, Toast }
+export default Toast
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  plugins: [
+    // '@babel/plugin-proposal-optional-chaining'
+  ],
   module: {
     rules: [
       {
